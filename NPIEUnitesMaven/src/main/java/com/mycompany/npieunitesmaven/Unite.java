2aa80bdf543f21b907c7bc8f4368e3c6f7182083
jocelyn44 <j.merlaud@hotmail.fr>
2014-06-24 20:14:34 +0200

    javadoc unites

--- NPIEUnitesMaven/src/main/java/com/mycompany/npieunitesmaven/Unite.java
+++ NPIEUnitesMaven/src/main/java/com/mycompany/npieunitesmaven/Unite.java
@@ -12,21 +12,28 @@ public class Unite {
 	private double decal; // coefficient de decallage
 
 	
-	//constructeur 
+	/**Constructeur de la classse unite
+         * @param pnom : le nom de l'unite
+         * @param pval : le coefficient multiplicateur de l'unite
+         * @param pDecal : le coefficient de decallage de l'unite
+         **/
 	public Unite(String pNom, double pVal, double pDecal){
 		nom = pNom;
 		coef=pVal;
 		decal=pDecal;
 	}
 	
+        /**Getter du coefficient de decalage**/
 	public double getDecal() {
 		return decal;
 	}
 	
+        /**Getter du nom de l'unite**/
 	public String getNom() {
 		return nom;
 	}
-
+        
+        /**Getter du coefficient multiplicateur**/
 	public double getVal() {
 		return coef;
 	}
